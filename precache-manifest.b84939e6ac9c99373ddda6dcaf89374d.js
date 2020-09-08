49bb76e4dc8cff5071550f10c7c1db4e31776b1b
weeraponak <weerapon.ak.61@ubu.ac.th>
2020-09-08 21:28:18 +0700

    Updates

---
+++ precache-manifest.b84939e6ac9c99373ddda6dcaf89374d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8f90c2f575dccf804858cf931a36e3c",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "1697f1e2c58053c5fbf3",
+    "url": "/Tic-Tac-Toe/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/Tic-Tac-Toe/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1697f1e2c58053c5fbf3",
+    "url": "/Tic-Tac-Toe/static/js/main.d878f48a.chunk.js"
+  },
+  {
+    "revision": "340fa9848be46dd5a3e7",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.e29fce77.js"
+  }
+]);
